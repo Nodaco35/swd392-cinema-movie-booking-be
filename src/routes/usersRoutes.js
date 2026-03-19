c986f105f2750f17f11fcbe8add474020157d7eb
Nodaco35 <ngodoancuong699668@gmail.com>
2026-03-19 17:23:39 +0700

    Fix: login and register

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
-import { getUsers, getUserById, createUser } from "../controllers/userController.js";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+} from "../controllers/userController.js";
+import { login, register } from "../controllers/authController.js";
 
 const router = Router();
+
+// Public routes
+router.post("/login", login);
+router.post("/register", register);
 
 router.get("/", getUsers);
 router.post("/", createUser);
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 import { User } from "../models/index.js";
+import bcrypt from "bcryptjs";
 
 export async function getAllUsers({ email, password } = {}) {
   if (email && password) {
@@ -10,10 +11,37 @@ export async function getAllUsers({ email, password } = {}) {
   return User.findAll();
 }
 
+export async function getUserByEmail(email) {
+  return User.findOne({ where: { email } });
+}
+
 export async function getUserById(id) {
   return User.findByPk(id);
 }
 
 export async function createUser({ full_name, email, password, phone, role }) {
-  return User.create({ full_name, email, password, phone, role });
+  // return User.create({ full_name, email, password, phone, role });
+
+  // Logic hash mật khẩu để đảm bảo an toàn
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  return User.create({
+    full_name,
+    email,
+    password: hashedPassword,
+    phone,
+    role: role || "customer",
+  });
+}
+
+export async function checkLogin(email, password) {
+  const user = await getUserByEmail(email);
+  if (!user) return null;
+
+  // So sánh mật khẩu người dùng nhập với mật khẩu đã mã hóa trong DB
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return null;
+
+  return user;
 }
